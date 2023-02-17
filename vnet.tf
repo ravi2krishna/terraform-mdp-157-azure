a01c2ca5c3d49eaf97b709c901918c13cc915cbe
ravi2krishna <ravi2krishna@gmail.com>
2023-02-17 20:32:17 +0530

    network secuirty group

--- vnet.tf
+++ vnet.tf
@@ -16,3 +16,24 @@ resource "azurerm_subnet" "ecomm-subnet" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
+resource "azurerm_network_security_group" "ecomm-nsg" {
+  name                = "ecomm-traffic"
+  location            = azurerm_resource_group.ecomm-rg.location
+  resource_group_name = azurerm_resource_group.ecomm-rg.name
+
+  security_rule {
+    name                       = "ecomm-rules"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = "dev"
+  }
+}
