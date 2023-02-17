935424ea94ef8e92ccb81843f80b3bef7d27cb30
ravi2krishna <ravi2krishna@gmail.com>
2023-02-17 20:25:42 +0530

    subnet

--- vnet.tf
+++ vnet.tf
@@ -8,3 +8,11 @@ resource "azurerm_virtual_network" "ecomm-vnet" {
     environment = "dev"
   }
 }
+
+resource "azurerm_subnet" "ecomm-subnet" {
+  name                 = "ecomm-subnet"
+  resource_group_name  = azurerm_resource_group.ecomm-rg.name
+  virtual_network_name = azurerm_virtual_network.ecomm-vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
